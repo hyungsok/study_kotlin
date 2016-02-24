1e788dc5e093306b2313ac8d429e15208c0bb5af
Hyungsok Lee <hyungsok7@gmail.com>
2016-02-24 21:48:37 +0900

    베이스 코드 추가

---
+++ tutorials/src/main/java/com/example/declaration/TestPair.kt
@@ -0,0 +1,25 @@
+package com.example.declaration
+
+/**
+ * This example introduces a concept that we call mutli-declarations.
+ * It creates multiple variable at once. Anything can be on the right-hand
+ * side of a mutli-declaration, as long as the required number of component
+ * functions can be called on it.
+ * See http://kotlinlang.org/docs/reference/multi-declarations.html#multi-declarations
+ */
+fun main(args: Array<String>) {
+    val pair = Pair(1, "one")
+    // multiple variable`
+    val (num, name) = pair
+    println("num = $num, name = $name")
+}
+
+class Pair<K, V>(val first: K, val second: V) {
+    operator fun component1(): K {
+        return first
+    }
+
+    operator fun component2(): V {
+        return second
+    }
+}
