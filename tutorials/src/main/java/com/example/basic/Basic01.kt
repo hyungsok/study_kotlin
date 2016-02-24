94806c6670f6456f97cdddcafa7b93432196d036
Hyungsok Lee <hyungsok7@gmail.com>
2016-02-24 21:34:07 +0900

    베이스 코드 추가

--- tutorials/src/main/java/com/example/basic/Basic01.kt
+++ tutorials/src/main/java/com/example/basic/Basic01.kt
@@ -4,3 +4,35 @@ package com.example.basic
  * Created by hyungsok on 2016. 2. 24..
  */
 
+fun main(args: Array<String>) {
+    // 다른 코틀린 파일에서 정의된 메소드를 호출할 수 있다
+    println(max(10, 5))
+
+    var x = 30
+    val y = 10
+    if (x in 1..y - 1)
+        println("OK")
+
+    //Iterate over a range:
+    for (a in 1..5)
+        print("${a} ")
+
+    //Check if a number is out of range:
+    println()
+    val array = arrayListOf<String>()
+    array.add("aaa")
+    array.add("bbb")
+    array.add("ccc")
+
+    if (x !in 0..array.size - 1)
+        println("Out: array has only ${array.size} elements. x = ${x}")
+
+    //Check if a collection contains an object:
+    if ("aaa" in array) // collection.contains(obj) is called
+        println("Yes: array contains aaa")
+
+    if ("ddd" in array) // collection.contains(obj) is called
+        println("Yes: array contains ddd")
+    else
+        println("No: array doesn't contains ddd")
+}
--- tutorials/src/main/java/com/example/basic/Basic02.kt
+++ tutorials/src/main/java/com/example/basic/Basic02.kt
@@ -1,5 +1,25 @@
 package com.example.basic
 
 /**
- * Created by kgkim on 2016. 2. 24..
+ * Created by hyungsoklee on 2016. 2. 24..
  */
+fun main(args: Array<String>) {
+    cases("Hello")
+    cases(1)
+    cases(System.currentTimeMillis())
+    cases(MyClass())
+    cases("hello")
+}
+
+fun cases(obj: Any) {
+    when (obj) {
+        1 -> println("One")
+        "Hello" -> println("Greeting")
+        is Long -> println("Long")
+        !is String -> println("Not a string")
+        else -> println("Unknown")
+    }
+}
+
+class MyClass() {
+}
